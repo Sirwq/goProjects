c6f7019b0e4141e9202ef7b6fb4fca69d14c4e49
Sirwq <vitaslav73@gmail.com>
2026-01-30 23:34:46 +0300

    funcs updated

--- funcs/funcs.go
+++ funcs/funcs.go
@@ -8,6 +8,21 @@ func main() {
 	fmt.Println(divmod(32, 3))
 	_, v := divmod(10, 20)
 	fmt.Println(v)
+
+	fmt.Println(strcon("blabla", " ", "bla"))
+
+	nextInt := intSeq()
+
+	fmt.Println(nextInt())
+	fmt.Println(nextInt())
+
+	wrapped := logger(func() {
+		fmt.Println(plus(4, 6))
+	})
+
+	wrapped()
+
+	loggerNoWrap("minus", minus(3, 2))
 }
 
 func plus(a int, b int) int {
@@ -22,4 +37,37 @@ func divmod(a, b int) (quotient float32, remainder int) {
 	quotient = float32(a) / float32(b)
 	remainder = a % b
 	return
+}
+
+func strcon(s ...string) string {
+	var ret []rune
+
+	for _, str := range s {
+		for _, ch := range str {
+			ret = append(ret, ch)
+		}
+	}
+	return string(ret)
+}
+
+func intSeq() func() int {
+	i := 0
+	return func() int {
+		i++
+		return i
+	}
+}
+
+func logger(f func()) func() {
+	return func() {
+		fmt.Println("start")
+		f()
+		fmt.Println("end")
+	}
+}
+
+func loggerNoWrap(name string, result any) {
+	fmt.Println("start")
+	fmt.Printf("%s: %v\n", name, result)
+	fmt.Println("end")
 }
