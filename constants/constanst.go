421e0faacb810b56a7185ee3b0fa7d8cbb47e66e
Sirwq <vitaslav73@gmail.com>
2026-01-30 19:47:59 +0300

    constants

---
+++ constants/constanst.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+const S string = "constant"
+
+func main() {
+	fmt.Println(S)
+
+	const N = 5000000
+	const D = 3e20 / N
+
+	fmt.Println(int64(D))
+	fmt.Println(math.Sin(N))
+}
