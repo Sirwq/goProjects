ea4f9eebf369ce0975823aab872a3a2e3dd60721
Sirwq <vitaslav73@gmail.com>
2026-02-12 10:58:11 +0300

    json struct

---
+++ choose-your-own-adventure/main.go
@@ -0,0 +1,10 @@
+package main
+
+type jsonToGo struct {
+	Title   string   `json:"title"`
+	Story   []string `json:"story"`
+	Options []struct {
+		Text string `json:"text"`
+		Arc  string `json:"arc"`
+	} `json:"options"`
+}
