c4cea1ea9ffa677d1b38085bc7c2ce3d37193aca
Sirwq <vitaslav73@gmail.com>
2026-02-23 05:40:33 +0300

    first commit

--- gophercies/HTML-Link-Parser/main.go
+++ gophercies/HTML-Link-Parser/main.go
@@ -5,40 +5,57 @@ import (
 	"io"
 	"log"
 	"os"
+	"strings"
 
 	"golang.org/x/net/html"
 )
 
 func main() {
-	f, err := os.Open("ex1.html")
+	f, err := os.Open("ex5.html")
 
 	if err != nil {
 		log.Fatal("Error while opening file")
 	}
 	defer f.Close()
 
+	var linkText strings.Builder
 	tokenizer := html.NewTokenizer(f)
-	insideLink := false
+	var counter int = 0
 
 	for {
+
 		tt := tokenizer.Next()
+		tagName := tokenizer.Token().Data
 
-		if tt == html.ErrorToken {
-			err := tokenizer.Err()
-			if err == io.EOF {
-				break
+		switch tt {
+		case html.StartTagToken:
+			token := tokenizer.Token()
+
+			if token.Data == "a" {
+				counter++
+				for _, attr := range token.Attr {
+					fmt.Println("href:", attr.Val)
+				}
 			}
-			return // err
-		}
+		case html.TextToken:
+			if (counter > 0 && counter < 2) && tagName == "a" {
+				linkText.WriteString(tokenizer.Token().Data)
+			}
+		case html.EndTagToken:
+			token := tokenizer.Token()
 
-		if tt == html.StartTagToken {
-			fmt.Print("Start tag: ")
-			fmt.Println(tokenizer.Token().Data)
-			//fmt.Println(tokenizer.Token())
-		}
+			if token.Data == "a" {
+				counter--
+				fmt.Println("Text:", strings.TrimSpace(linkText.String()))
+				linkText.Reset()
+			}
+		case html.ErrorToken:
+			err := tokenizer.Err()
 
-		if tt == html.EndTagToken {
-			fmt.Print("End tag")
+			if err == io.EOF {
+				return
+			}
+
 		}
 
 	}
